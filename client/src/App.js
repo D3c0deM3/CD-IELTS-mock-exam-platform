b05adf247d109b96a31b60a66b4784c3f7a6b32d
D3c0deM3 <sharipov06shoxrux@gmail.com>
2025-12-16 17:31:44 +0500

    added hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import StartScreen from "./pages/StartScreen";
 import PendingScreen from "./pages/PendingScreen";
 import Login from "./pages/Login";
