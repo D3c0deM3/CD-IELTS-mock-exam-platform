ecb8612ba57d6babbfc333ff424f31ac79b1b9c4
D3c0deM3 <sharipov06shoxrux@gmail.com>
2025-12-16 16:13:08 +0500

    db-configured

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "cd_mock",
+  host: process.env.MYSQL_HOST || "127.0.0.1",
+  port: process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 3306,
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "0000",
+  database: process.env.MYSQL_DATABASE || "cd_mock",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
